4df96bba460f864f08d4c3e5b7ee346efa9b6d6c
osboxes <arnab.dey@temple.edu>
2022-06-23 11:43:20 -0400

    commit 4

--- svf2ftdi.js
+++ svf2ftdi.js
@@ -83,30 +83,11 @@ if (process.stdin.isTTY) {
 
 const { Readable } = require('stream');
 
-/**
- * @param binary Buffer
- * returns readableInstanceStream Readable
- */
-function bufferToStream(binary) {
-
-    const readableInstanceStream = new Readable({
-        read() {
-            this.push(binary);
-            this.push(null);
-        }
-    });
-
-    return readableInstanceStream;
-}
-
 if (source) {
     const jtag = ftdi(options);
     const s1 = svf(jtag);
 
-    source.on('data', chunk => {
-        let rs = bufferToStream(chunk);
-        rs.pipe(s1);
-    });
+    source.pipe(s1)
 
     //on('data', (chunk) => {
     //    s1;
